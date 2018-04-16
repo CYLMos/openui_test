ba2ef5f547e988e547d19eab55c52f99a2d7c398
LCY <cylin910021@gmail.com>
2018-04-16 11:52:27 +0800

    Adjust the architecture of view and controller folder

--- controller/Controller/App.controller.js
+++ controller/Controller/App.controller.js
@@ -4,10 +4,10 @@ sap.ui.define([
     "use strice";  /// Use strict mode in the browser
 
     /* "sap.ui.demo.walkthrough" is the root name
-     * .controller represents in the folder "controller"
+     * .controller.Controller represents in the folder "controller/Controller"
      * .App respresents the controller name(.js file name)
     */
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+    return Controller.extend("sap.ui.demo.walkthrough.controller.Controller.App", {
         onShowHello : function(){
             /// Show "Hello World" in alert
             alert('Hello World');
