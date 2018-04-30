e101e402ba6d14349cf2499038067a4310ca1f7a
LCY <cylin910021@gmail.com>
2018-04-30 11:49:59 +0800

    Add master file

---
+++ case2/controller/App.controller.js
@@ -0,0 +1,120 @@
+sap.ui.define([
+    "jquery.sap.global", /// Define jquery sap
+    "sap/ui/core/mvc/Controller", /// Define controller
+    "sap/ui/model/json/JSONModel"
+], function(jQuery, Controller, JSONModel) {  /// Then implement it
+    "use strice";  /// Use strict mode in the browser
+
+    /* "sap.ui.demo.walkthrough" is the root name
+     * .controller.Controller represents in the folder "controller/Controller"
+     * .App respresents the controller name(.js file name)
+    */
+
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+        onInit : function(){
+            var jsonData = window.sessionStorage.getItem("json");
+            if(jsonData == null || 
+                jsonData == undefined)
+            {
+                var data = {
+                    "Data" : [
+                        {"number" : "0", "name" : "name 0", "explain" : "explain 0"},
+                        {"number" : "1", "name" : "name 1", "explain" : "explain 1"}
+                    ]
+                };
+                var model = new JSONModel(data);
+                this.byId('table').setModel(model, 'model');
+    
+                window.sessionStorage.setItem("json", model.getJSON());
+            }
+            else{
+                var model = new JSONModel(JSON.parse(jsonData));
+                this.byId('table').setModel(model, 'model');
+            }
+        },
+
+        selectionChange : function(oEvent){
+            var items = oEvent.getParameter("listItem");
+            var model = items.getBindingContext('model').getObject();
+            window.sessionStorage.setItem('passData', JSON.stringify(model));
+        },
+
+        research : function(){
+            window.sessionStorage.setItem("passStatus", null);
+            window.sessionStorage.setItem("passData", null);
+
+            var json = window.sessionStorage.getItem('json');
+            var model = new JSONModel(JSON.parse(json));
+
+            this.byId('table').setModel(model, 'model');
+        },
+
+        modify : function(){
+            if(window.sessionStorage.getItem('passData') == null ||
+               window.sessionStorage.getItem('passData') == undefined ||
+               window.sessionStorage.getItem('passData') == 'null')
+            {
+                alert('Choose a row data!');
+            }
+
+            else{
+                var data = {
+                    "status" : "modify"
+                };
+                
+                window.sessionStorage.setItem("passStatus", JSON.stringify(data));
+                window.location = "edit.html";
+            }
+        },
+
+        add : function(){
+            var data = {
+                "status" : "add"
+            };
+            window.sessionStorage.setItem("passStatus", JSON.stringify(data));
+            window.location = "edit.html";
+        },
+
+        delete : function(){
+            if(window.sessionStorage.getItem('passData') == null ||
+               window.sessionStorage.getItem('passData') == undefined ||
+               window.sessionStorage.getItem('passData') == 'null')
+            {
+                alert('Choose a row data!');
+            }
+            else{
+                var jsonData = JSON.parse(window.sessionStorage.getItem("json"));
+                var selectData = JSON.parse(window.sessionStorage.getItem('passData'));
+
+                for(var i = 0; i < jsonData.Data.length; i++){
+                    if(jsonData.Data[i].number == selectData.number &&
+                       jsonData.Data[i].name == selectData.name)
+                    {
+                        delete jsonData.Data[i];
+                        break;
+                    }
+                }
+
+                jsonData = replacer(jsonData);
+
+                var model = new JSONModel(jsonData);
+                this.byId('table').setModel(model, 'model');
+
+                window.sessionStorage.setItem('json', JSON.stringify(jsonData));
+            }
+        }
+    });
+});
+
+function replacer(jsonData){
+    var data = {
+        Data : []
+    }
+    for(var i = 0; i < jsonData.Data.length; i++){
+        if(jsonData.Data[i] != null || jsonData.Data[i] != undefined){
+            data.Data.push(jsonData.Data[i]);
+        }
+    }
+
+    return data;
+}
---
+++ case2/controller/App2.controller.js
@@ -0,0 +1,62 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller", /// Define controller
+    "sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {  /// Then implement it
+    "use strice";  /// Use strict mode in the browser
+
+    /* "sap.ui.demo.walkthrough" is the root name
+     * .controller.Controller represents in the folder "controller/Controller"
+     * .App respresents the controller name(.js file name)
+    */
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App2", {
+        onInit : function(){
+            var jsonStatus = JSON.parse(window.sessionStorage.getItem("passStatus"));
+            var jsonData = JSON.parse(window.sessionStorage.getItem("passData"));
+
+            if(jsonStatus.status == 'modify'){
+                this.byId('input_number').setEditable(false);
+
+                var model = new JSONModel(jsonData);
+                this.getView().setModel(model);
+            }
+        },
+
+        back : function(){
+            window.sessionStorage.setItem("passStatus", null);
+            window.sessionStorage.setItem("passData", null);
+            window.location = "index.html";
+        },
+
+        save : function(){
+            window.sessionStorage.setItem("passData", null);
+
+            var jsonStatus = JSON.parse(window.sessionStorage.getItem("passStatus"));
+            var jsonData = JSON.parse(window.sessionStorage.getItem("json"));
+
+            var saveData = {
+                "number" : this.byId('input_number').getValue(),
+                "name" : this.byId('input_name').getValue(),
+                "explain" : this.byId('ta_explain').getValue()
+            }
+
+            if(jsonStatus.status == 'modify'){
+                for(var i = 0; i < jsonData.Data.length; i++){
+                    if(jsonData.Data[i].number == saveData.number){
+                        jsonData.Data[i].name = saveData.name;
+                        jsonData.Data[i].explain = saveData.explain;
+
+                        break;
+                    }
+                }
+            }
+            else if(jsonStatus.status == 'add'){
+                jsonData.Data[jsonData.Data.length] = saveData;
+            }
+            
+            window.sessionStorage.setItem("json", JSON.stringify(jsonData));
+            alert('Save success!');
+
+            window.location = "index.html";
+        }
+    });
+});
